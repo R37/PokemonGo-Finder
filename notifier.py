f829e2078503f06956de946e6b679708debc2e35
R37 <karltrueman1997@gmail.com>
2016-07-22 22:29:09 +0100

    Added Pushbullet channel support
    This should use the first channel created on the given pushbullet api keys account

--- notifier.py
+++ notifier.py
@@ -4,35 +4,39 @@ from datetime import datetime
 import sys
 
 # Fixes the encoding of the male/female symbol
-reload(sys)
+reload(sys)  
 sys.setdefaultencoding('utf8')
 
 pushbullet_client = None
 wanted_pokemon = None
 unwanted_pokemon = None
+pb = None
+my_channel = None
 
 # Initialize object
 def init():
-    global pushbullet_client, wanted_pokemon, unwanted_pokemon
+    global pushbullet_client, wanted_pokemon, unwanted_pokemon, pb, my_channel
     # load pushbullet key
     with open('config.json') as data_file:
         data = json.load(data_file)
         # get list of pokemon to send notifications for
         if "notify" in data:
             wanted_pokemon = _str( data["notify"] ) . split(",")
-
+            
             # transform to lowercase
             wanted_pokemon = [a.lower() for a in wanted_pokemon]
         #get list of pokemon to NOT send notifications for
         if "do_not_notify" in data:
             unwanted_pokemon = _str( data["do_not_notify"] ) . split(",")
-
+            
             # transform to lowercase
             unwanted_pokemon = [a.lower() for a in unwanted_pokemon]
         # get api key
         api_key = _str( data["pushbullet"] )
         if api_key:
             pushbullet_client = Pushbullet(api_key)
+            pb = Pushbullet(api_key)
+            my_channel = pb.channels[0]
 
 
 # Safely parse incoming strings to unicode
@@ -59,9 +63,10 @@ def pokemon_found(pokemon):
 
     notification_text = "Pokemon Finder found " + _str(pokemon["name"]) + "!"
     disappear_time = str(datetime.fromtimestamp(pokemon["disappear_time"]).strftime("%I:%M%p").lstrip('0'))+")"
-    location_text = _str(pokemon["name"]) + " will be available until " + disappear_time + "."
+    location_text = "Locate on Google Maps : " + google_maps_link + ". " + _str(pokemon["name"]) + " will be available until " + disappear_time + "."
 
     push = pushbullet_client.push_link(notification_text, google_maps_link, body=location_text)
+    push = my_channel.push_link( _str(pokemon["name"] + " found, Available until " +disappear_time ) , google_maps_link )
 
 
 
